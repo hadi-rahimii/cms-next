7f86a0f16f1719eeee2597b27ca442d5b0115cf7
hadi-rahimii <0hadi.rahimi@gmail.com>
2024-02-29 15:52:17 +0330

    connect to mongoo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import Course from "@/components/templates/index/Course";
+import coursesModel from "@/models/course";
+import connectToDB from "@/utils/db";
 
 const index = () => {
   return <Course />;
 };
+
+export async function getStaticProps(context) {
+  connectToDB();
+  const courses = await coursesModel.find({});
+
+  console.log(courses);
+
+  return {
+    props: {},
+  };
+}
 
 export default index;
