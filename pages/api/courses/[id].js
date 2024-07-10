d9b2d551576c0bddf932282bd9e4edc18a810984
hadi-rahimii <0hadi.rahimi@gmail.com>
2024-07-10 19:30:07 +0330

    put method

--- pages/api/courses/[id].js
+++ pages/api/courses/[id].js
@@ -7,17 +7,36 @@ connectToDB()
 
 if (req.method === 'DELETE') {
     const {id} = req.query;
+
     if(isValidObjectId(id)){
        try {
          await courseModel.findOneAndDelete({_id : id})
          return res.json({message : 'course removed successfuly :)'})
        } catch (error) {
-        return res.status(500).json({message : 'internal server error !!'})
+        return res.status(500).json({message : 'internal server error '})
        }
     }else{
         return res.status(422).json({message : 'Course Id is not valid'})
     }
-} 
+} else if(req.method === 'PUT') {
+    const {id} = req.query;
+    const {title} = req.body
+    if(isValidObjectId(id)){
+        try {
+         await courseModel.findOneAndUpdate({_id : id},{title})
+         return res.json({message : 'course updated successfuly :)'})
+         
+            
+        } catch (error) {
+        return res.status(500).json({message : 'internal error server'})
+            
+        }
+    }else{
+        return res.status(422).json({message : 'Course Id is not valid'})
+
+    }
+
+}
 
 
 }
